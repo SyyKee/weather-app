020b32babf80e7c4cc439735d189684fc288a727
SyyKee <83742580+SyyKee@users.noreply.github.com>
2023-05-02 16:45:39 +0100

    Update api.service.ts

--- weather/src/app/service/api.service.ts
+++ weather/src/app/service/api.service.ts
@@ -2,8 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, } from 'rxjs';
 
-//const apiKey = "bd93fbb6a8f44f40a2392732231704"
-const apiKey = "2b07174b1c474d77aa481611230205"
+
+const apiKey = "YOUR_KEY"
 
 const url = "http://api.weatherapi.com/v1/forecast.json?key="
 
